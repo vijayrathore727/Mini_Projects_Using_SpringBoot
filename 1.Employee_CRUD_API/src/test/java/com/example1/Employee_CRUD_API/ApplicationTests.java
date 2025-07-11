f823309a6ffac4ec9fbf56b4b4c0dc590cfd8446
VIJAY <68022329+vijayrathore727@users.noreply.github.com>
2025-07-11 20:19:54 +0530

    Add files via upload

---
+++ 1.Employee_CRUD_API/src/test/java/com/example1/Employee_CRUD_API/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example1.Employee_CRUD_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
